4a4f5bdd907f64eae9247f2005bc0642925b47dd
Olena Klementieva <foksysmile@gmail.com>
2023-12-06 12:58:40 +0200

    onclick

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,34 @@
-import '../../script/test'
+document.querySelector('.form__button').onclick = () => {
+    const form = document.getElementById('form')
 
-console.log('container')
+    const email = document.getElementById('email')
+
+    console.log(email.checkValidity())
+
+    if(!email.checkValidity()) {
+        if(email.value.length === 0) {
+            email.setCustomValidity('Помилка. Введіть значення.')
+        }
+
+        if(email.value.length > 10) {
+            email.setCustomValidity('Помилка. Введіть до 10 символів.')
+        }
+
+        email.reportValidity()
+    }
+}
+
+document.querySelectorAll('form__button--add').forEach((elem) => {
+    elem.onclick = () => {
+        if(elem.getAttribute('operator') === '+') {
+            return age.stepUp(Number(elem.getAttribute('value')))
+        }
+
+        if(elem.getAttribute('operator') === '-') {
+            return age.stepDown(Number(elem.getAttribute('value')))
+        }
+    
+       
+    }
+})
+
